ce7ecc9b23e35899fb9613126689ee095e45ae0d
Brian Michel <bmoralescutting@gmail.com>
2026-02-17 21:33:29 -0500

    feat: add project entity

--- src/modules/project/domain/entities/project.ts
+++ src/modules/project/domain/entities/project.ts
@@ -1,6 +1,14 @@
+import type { Category } from "../../../category/domain/entities/category";
+import type { Comment } from "../../../comment/domain/entities/comment";
+import type { Like } from "../../../like/domain/entities/like";
+
 export interface Project {
   id: string;
   title: string;
   description: string;
   imagesUrl?: string[];
+  likes?: Like[];
+  comments?: Comment[];
+  categories?: Category[];
+  createdAt?: Date[];
 }
---
+++ src/modules/project/dto/read/project.ts
@@ -0,0 +1,12 @@
+import type { UserResponse } from "../../../user/dto/read/user";
+
+export interface ProjectResponse {
+  id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  imagesUrl: string[];
+  user: UserResponse;
+  likesCount: number;
+  commentsCount: number;
+}
