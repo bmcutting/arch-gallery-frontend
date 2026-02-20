ad015256e3574167a8b7c144f6340670ff8b5830
Brian Michel <bmoralescutting@gmail.com>
2026-02-20 15:50:37 -0500

    fix: redirect to Login when user not found

--- src/containers/Profile/hooks/useProfile.tsx
+++ src/containers/Profile/hooks/useProfile.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { getMe } from "../../../modules/user/services/get-me";
 import type { User } from "../../../modules/user/domain/entities/user";
+import { APP_ROUTES } from "../../../modules/app/domain/constants/app-routes";
 
 export default function useProfile() {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User>();
 
   useEffect(() => {
     getMe()
@@ -11,7 +12,7 @@ export default function useProfile() {
         setUser(data);
       })
       .catch(() => {
-        setUser(null);
+        window.location.href = APP_ROUTES.LOGIN;
       });
   }, [user?.id]);
 
