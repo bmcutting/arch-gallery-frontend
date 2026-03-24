ad72ee24eed03dc5d74da2d4ca0e53e781a893c9
Brian Michel <bmoralescutting@gmail.com>
2026-03-23 20:40:29 -0400

    partial commit

--- src/modules/project/services/update-project.ts
+++ src/modules/project/services/update-project.ts
@@ -2,5 +2,6 @@ import { instance } from "../../app/modules/http/domain/instance";
 import type { UpdateProjectDto } from "../dto/write/update-project";
 
 export function updateProject(props: UpdateProjectDto): Promise<boolean> {
+  console.log(props);
   return instance.put<boolean>("projects", props).then((res) => res.data);
 }
