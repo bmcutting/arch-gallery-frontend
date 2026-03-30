32aeeeece0934aa875f44189777657ff14ea9822
Brian Michel <bmoralescutting@gmail.com>
2026-03-30 17:54:34 -0400

    fix: add long bio in about tab

--- src/containers/Profile/components/AboutTab/AboutTab.tsx
+++ src/containers/Profile/components/AboutTab/AboutTab.tsx
@@ -13,7 +13,7 @@ export default function AbouTab({ user }: Props) {
         </h2>
         <div className="prose prose-stone max-w-none animate-fadeIn">
           <p className="text-sm md:text-base lg:text-lg text-gray-700 leading-relaxed tracking-wide">
-            {user?.longBio}
+            {user?.longBio || "Aún no hay biografía disponible."}
           </p>
         </div>
       </section>
