f03529ff07ae9ba3fa1a454af4b6f9d3536522d8
Brian Michel <bmoralescutting@gmail.com>
2026-03-30 21:16:33 -0400

    feat: add CollapsibleSection component

---
+++ src/modules/app/modules/shared/components/CollapsibleSection/CollapsibleSection.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+
+interface CollapsibleSectionProps {
+  title: string;
+  children: React.ReactNode;
+  defaultOpen?: boolean;
+}
+
+export default function CollapsibleSection({
+  title,
+  children,
+  defaultOpen,
+}: CollapsibleSectionProps) {
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+
+  return (
+    <div className="border-b border-border last:border-b-0 py-4">
+      <button
+        type="button"
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex justify-between items-center w-full text-left"
+      >
+        <h2 className="text-lg md:text-xl font-semibold">{title}</h2>{" "}
+        {isOpen ? <FaChevronUp /> : <FaChevronDown />}
+      </button>
+      {isOpen && <div className="mt-4">{children}</div>}
+    </div>
+  );
+}
